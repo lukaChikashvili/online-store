ee2d325f7b73b83b4b16fbc8f4621bd11513cc3d
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-05 17:13:38 +0400

    add connect to mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
-console.log('hellp sf')
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const cookieParser = require('cookie-parser');
+const connectDB  = require('./config/db');
+
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
+
+app.listen(port, () => console.log('server is running'));
+
+
