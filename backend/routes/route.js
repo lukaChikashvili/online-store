9e170188c7e806216df5cca580ed4cbf982c5325
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-08 16:47:59 +0400

    update currnet user profile

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -3,14 +3,15 @@ const { createUser,
         loginUser, 
         getAllUsers,
         logoutUser, 
-        getCurrentProfile } = require('../controllers/actions');
-        
+        getCurrentProfile, 
+        updateCurrentProfile} = require('../controllers/actions');
+
 const { authenticate, authorizeAdmin } = require('../middlewares/authHandler');
 const router = express.Router();
 
 router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth', loginUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(authenticate, getCurrentProfile )
+router.route('/profile').get(authenticate, getCurrentProfile).put(authenticate, updateCurrentProfile);
 
 module.exports = {routes: router}
