0f68deb9165dd0da1398d65aedd79242f5afa0c3
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-08 16:25:01 +0400

    add specific user route and function

--- backend/controllers/actions.js
+++ backend/controllers/actions.js
@@ -88,10 +88,26 @@ const getAllUsers = asyncHandler(async(req, res) => {
 
 })
 
+// specific user
+const getCurrentProfile = asyncHandler(async(req, res) => {
+     const user = await User.findById(req.user._id);
+
+     if(user) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        });
+     }else {
+        res.status(404);
+        throw new Error('user not found');
+     }
+})
 
 module.exports = {
     createUser,
     loginUser,
     logoutUser,
-    getAllUsers
+    getAllUsers,
+    getCurrentProfile
 }
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { createUser, loginUser, getAllUsers, logoutUser } = require('../controllers/actions');
+const { createUser,
+        loginUser, 
+        getAllUsers,
+        logoutUser, 
+        getCurrentProfile } = require('../controllers/actions');
+        
 const { authenticate, authorizeAdmin } = require('../middlewares/authHandler');
 const router = express.Router();
 
 router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth', loginUser);
 router.post('/logout', logoutUser);
+router.route('/profile').get(authenticate, getCurrentProfile )
 
 module.exports = {routes: router}
