b54f10ef8159cd86ff6429588b9dc691554ae687
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-13 14:28:01 +0400

    add blog mongoose model

---
+++ backend/models/blogModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+
+
+const blogSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    comment: {type: String, required: true},
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    image: {type: String, required: true},
+
+
+}, {timestamps: true});
+
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = {Blog};
+
--- frontend/store/src/components/Login.jsx
+++ frontend/store/src/components/Login.jsx
@@ -43,36 +43,36 @@ const Login = () => {
 
   return (
     <div>
-      <section className='pl-[10rem] flex flex-col flex-wrap'>
+      <section className='pl-[10rem] flex flex-col flex-wrap items-center'>
          <div className='mr-[4rem] mt-[5rem]'>
-             <h1 className='text-6xl font-semibold mb-4'>Sign In</h1>
+             <h1 className='text-5xl font-semibold mb-4'> სისტემაში შესვლა</h1>
             </div>
 
             <form onSubmit={submitHandler} className='container w-[40rem]'>
               <div className='my-[2rem] flex flex-col gap-4'>
-                 <label htmlFor='email' className='block text-sm font-medium'>Email adress</label>
+                 <label htmlFor='email' className='block text-sm font-medium'>ელ-ფოსტა</label>
 
                  <input type='email'
                         value={email}
                         className='bg-gray-200 w-full mt-1 p-2 rounded outline-none shadow-lg'
                         onChange={(e) => setEmail(e.target.value)}         
                                />
-                 <label htmlFor='password' className='block text-sm font-medium'>Password</label>
+                 <label htmlFor='password' className='block text-sm font-medium'>პაროლი</label>
                  <input type='password'
                         value={password}
                         className='bg-gray-200 w-full mt-1 p-2 rounded outline-none shadow-lg'
                         onChange={(e) => setPassword(e.target.value)}         
                                />
               </div>
-              <button disabled = {isLoading} type='submit' className='bg-orange-400 text-white w-full px-4 py-2 
-              rounded shadow-gray-400 shadow-md duration-500 ease hover:bg-orange-300'>{isLoading ? "Signing In.." : "Sign In"}</button>
+              <button disabled = {isLoading} type='submit' className='bg-blue text-white w-full px-4 py-2 
+              rounded shadow-gray-400 shadow-md duration-500 ease hover:bg-orange-300'>{isLoading ? "გთხოვთ დაელოდოთ.." : "შესვლა"}</button>
             </form>
 
             <div className='mt-4'>
                 <p >
-                   New Customer ? {" "} <Link to ={redirect ? `/register?redirect=${redirect}` : '/register'} className='text-orange-600 underline'
+                   ახალი წევრი ? {" "} <Link to ={redirect ? `/register?redirect=${redirect}` : '/register'} className='text-orange-600 underline'
                    
-                   >Register</Link>
+                   >რეგისტრაცია</Link>
                 </p>
             </div>
       </section>
