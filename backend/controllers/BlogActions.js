45ebbd3ed6bf81e4580a500bdd3036e8b4700fc2
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-13 15:08:20 +0400

    add blog update

--- backend/controllers/BlogActions.js
+++ backend/controllers/BlogActions.js
@@ -1,6 +1,7 @@
 const { asyncHandler }  = require('../middlewares/asyncHandler');
 const { Blog } = require('../models/blogModel');
 
+// create blog
 const createBlog = asyncHandler(async(req, res) => {
    const { name, text } = req.fields;
      
@@ -27,7 +28,36 @@ const createBlog = asyncHandler(async(req, res) => {
 
 });
 
+// update blog
+const updateBlog = asyncHandler(async(req, res) => {
+    try {
+        
+        const { name, text } = req.fields;
+     
+        
+          // validation
+        switch(true) {
+         case !name:
+            return res.json({error: "Name is required"})
+            case !text:
+            return res.json({error: "text is required"})
+            
+            
+      }
+
+      const blog = await Blog.findByIdAndUpdate(req.params.id, {...req.fields}, {new: true});
+     await blog.save();
+      res.json(blog);
+
+
+
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+})
+
 
 module.exports = {
-    createBlog
+    createBlog,
+    updateBlog
 }
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -3,13 +3,14 @@ const formidable = require('express-formidable');
 const router = express.Router();
 const { authenticate, authorizeAdmin } = require('../middlewares/authHandler');
 const { checkId } = require('../middlewares/checkId');
-const { createBlog } = require('../controllers/BlogActions');
+const { createBlog, updateBlog } = require('../controllers/BlogActions');
 
 
 
 
 
 router.route('/').post(authenticate, authorizeAdmin, formidable(), createBlog);
+router.route('/:id').put(authenticate, authorizeAdmin, formidable(), updateBlog);
 
 
 
