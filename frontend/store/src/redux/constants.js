2bcfbeb11d3119dd7457eb594cd4fb3c7507d638
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-15 03:03:32 +0400

    add base url

--- frontend/store/src/redux/constants.js
+++ frontend/store/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://baia-jir6.onrender.com/";
 export const USERS_URL = "/api/"
 export const BLOGS_URL = "/api/blogs"
 export const UPLOAD_URL = "/api/upload"
