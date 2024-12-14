190a0535e38eeff6260c7b4255d59340ad92dd31
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-15 03:24:24 +0400

    add base url to constants

--- frontend/store/src/redux/constants.js
+++ frontend/store/src/redux/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://baia-jir6.onrender.com/";
-export const USERS_URL = "/api/"
-export const BLOGS_URL = "/api/blogs"
-export const UPLOAD_URL = "/api/upload"
-export const APPLY_URL = "/api/apply"
+export const USERS_URL = `${BASE_URL}/api/`;
+export const BLOGS_URL = `${BASE_URL}/api/blogs`;
+export const UPLOAD_URL = `${BASE_URL}/api/upload`;
+export const APPLY_URL = `${BASE_URL}/api/apply`;
