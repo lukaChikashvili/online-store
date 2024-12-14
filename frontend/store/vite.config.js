cfed0761957dcf5ddbd1754a58f1557d621f39b2
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-15 03:55:22 +0400

    add vite

--- frontend/store/vite.config.js
+++ frontend/store/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://baia-jir6.onrender.com/",
-      "/uploads/": "https://baia-jir6.onrender.com/",
+      "/api/": "https://baia-jir6.onrender.com",
+      "/uploads/": "https://baia-jir6.onrender.com",
     }
   }
 })
