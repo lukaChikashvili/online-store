992fe902ed5d157f94b1f6e9384d6cebc95e0ba8
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-17 03:35:20 +0400

    cors

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors({
   origin: ['http://localhost:5173', 'https://baia-frontend.onrender.com'],  
     methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   }));
 
