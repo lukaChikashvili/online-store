42be9eec2fe5ef23289682a148a230bd97c018ab
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-16 19:36:20 +0400

    add token

--- backend/middlewares/authHandler.js
+++ backend/middlewares/authHandler.js
@@ -10,6 +10,7 @@ const authenticate = asyncHandler(async(req, res, next) => {
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
         req.user = await User.findById(decoded.userId).select("-password");
         next();
     } catch (error) {
