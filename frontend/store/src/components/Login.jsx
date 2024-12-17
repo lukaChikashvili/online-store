1a94ac022c2e2511bc7c272e77aa5cad6d19b92a
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-17 20:09:38 +0400

    add password eye on input

--- frontend/store/src/components/Login.jsx
+++ frontend/store/src/components/Login.jsx
@@ -4,7 +4,8 @@ import { useLocation, useNavigate } from 'react-router';
 import { useLoginMutation } from '../redux/api/userSlice';
 import { Link } from 'react-router-dom';
 import { setCredentials } from '../redux/features/auth/authSlice';
-import Loader from './Loader';
+import { Eye, EyeClosed, EyeOff } from 'lucide-react';
+
 
 
 const Login = () => {
@@ -36,7 +37,7 @@ const Login = () => {
 
         try {
           const res = await login({ email, password }).unwrap();
-          console.log(res.token)
+          
             
           if (res.token) {
             
@@ -51,10 +52,12 @@ const Login = () => {
      }
     }
 
+    const [eye, setEye] = useState(false);
+
 
   return (
     <div>
-      <section className='pl-[10rem] flex flex-col flex-wrap items-center'>
+      <section className=' flex flex-col flex-wrap items-center'>
          <div className='mr-[4rem] mt-[5rem]'>
              <h1 className='text-5xl font-semibold mb-4'> სისტემაში შესვლა</h1>
             </div>
@@ -69,11 +72,18 @@ const Login = () => {
                         onChange={(e) => setEmail(e.target.value)}         
                                />
                  <label htmlFor='password' className='block text-sm font-medium'>პაროლი</label>
-                 <input type='password'
-                        value={password}
-                        className='bg-gray-200 w-full mt-1 p-2 rounded outline-none shadow-lg'
-                        onChange={(e) => setPassword(e.target.value)}         
-                               />
+                 <div className="relative">
+                 <input 
+                   type={eye ? 'text' : 'password'}
+                   value={password}
+                   className='bg-gray-200 w-full mt-1 p-2 rounded outline-none shadow-lg ' 
+                   onChange={(e) => setPassword(e.target.value)} 
+                 />
+                 <span className='absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer' onClick={() => setEye(!eye)}>
+                 {eye ? <EyeOff /> :  <Eye /> } 
+                 </span>
+               </div>
+
               </div>
               <button disabled = {isLoading} type='submit' className='bg-blue text-white w-full px-4 py-2 
               rounded shadow-gray-400 shadow-md duration-500 ease hover:bg-orange-300'>{isLoading ? "გთხოვთ დაელოდოთ.." : "შესვლა"}</button>
--- frontend/store/src/components/Register.jsx
+++ frontend/store/src/components/Register.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
 import { useRegisterMutation } from '../redux/api/userSlice';
 import { setCredentials } from '../redux/features/auth/authSlice';
+import { Eye, EyeOff } from 'lucide-react';
 
 const Register = () => {
     const [username, setUsername] = useState('');
@@ -45,18 +46,19 @@ const Register = () => {
         }
      }
     
+     const [eye, setEye] = useState(false);
 
   return (
     <div>
-         <section className='pl-[10rem] flex flex-col flex-wrap'>
+         <section className='flex flex-col flex-wrap items-center'>
          <div className='mr-[4rem] mt-[5rem]'>
-             <h1 className='text-6xl font-semibold mb-4'>Register</h1>
+             <h1 className='text-5xl font-semibold mb-4'>რეგისტრაცია</h1>
             </div>
 
             <form onSubmit={handleRegister}  className='container w-[40rem]'>
               <div className='my-[2rem] flex flex-col gap-4'>
 
-              <label htmlFor='username' className='block text-sm font-medium'>Username</label>
+              <label htmlFor='username' className='block text-sm font-medium'>სახელი</label>
 
               <input type='text'
                     value={username}
@@ -66,22 +68,28 @@ const Register = () => {
 
 
 
-                 <label htmlFor='email' className='block text-sm font-medium'>Email adress</label>
+                 <label htmlFor='email' className='block text-sm font-medium'>ელ-ფოსტა</label>
 
                  <input type='email'
                         value={email}
                         className='bg-gray-200 w-full mt-1 p-2 rounded outline-none shadow-lg'
                         onChange={(e) => setEmail(e.target.value)}         
                                />
-                 <label htmlFor='password' className='block text-sm font-medium'>Password</label>
-                 <input type='password'
-                        value={password}
-                        className='bg-gray-200 w-full mt-1 p-2 rounded outline-none shadow-lg'
-                        onChange={(e) => setPassword(e.target.value)}         
-                               />
+                 <label htmlFor='password' className='block text-sm font-medium'>პაროლი</label>
+                 <div className="relative">
+                 <input 
+                   type={eye ? 'text' : 'password'}
+                   value={password}
+                   className='bg-gray-200 w-full mt-1 p-2 rounded outline-none shadow-lg ' 
+                   onChange={(e) => setPassword(e.target.value)} 
+                 />
+                 <span className='absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer' onClick={() => setEye(!eye)}>
+                 {eye ? <EyeOff /> :  <Eye /> } 
+                 </span>
+               </div>
               </div>
 
-              <label htmlFor='password' className='block text-sm font-medium'>confirm password</label>
+              <label htmlFor='password' className='block text-sm font-medium'>დაადასტურეთ პაროლი</label>
                  <input type='password'
                         value={confirmPassword}
                         className='bg-gray-200 w-full mt-1 p-2 rounded outline-none shadow-lg'
@@ -90,8 +98,8 @@ const Register = () => {
 
 
               
-              <button disabled = {isLoading} type='submit' className='bg-orange-400 mt-8 text-white w-full px-4 py-2 
-              rounded shadow-gray-400 shadow-md duration-500 ease hover:bg-orange-300'>{isLoading ? "Registering.." : "Register"}</button>
+              <button disabled = {isLoading} type='submit' className='bg-blue mt-8 text-white w-full px-4 py-2 
+              rounded shadow-gray-400 shadow-md duration-500 ease hover:bg-orange-300'>{isLoading ? "მიმდინარეობს რეგისტრაცია.." : "რეგისტრაცია"}</button>
             </form>
 
           
