0bf4284805d3a9551b1bc703d5b40e56e9e12ac4
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-18 19:43:57 +0400

    add no blogs

--- frontend/store/src/components/LatestBlog.jsx
+++ frontend/store/src/components/LatestBlog.jsx
@@ -16,15 +16,19 @@ const LatestBlog = () => {
        <h1 className='text-3xl font-bold text-white'>უახლესი ბლოგი</h1>
          
          <div className='bg-white p-12 mt-12 rounded-md shadow-lg flex gap-8'>
-            {latestBlogs?.map((value) => (
-                 <Link key = {value._id} to = {`/blogs/detail/${value._id}`}>
-                 <div className='w-[20rem] flex flex-col gap-2 '>
-                      <img src = {`${BASE_URL}${value.image}`} className='w-[20rem] h-[20rem] rounded-md shadow-lg cursor-pointer object-cover'/>
-                      <h1 className='text-blue font-bold text-xl cursor-pointer duration-500 ease hover:underline'>{value.name}</h1>
-                      <p>{value.text.substring(0, 25)}...</p>
-                     </div>
-                     </Link>
-            ))}
+            {blogs?.length === 0 ?  (<span>პოსტები არ არის</span>) : 
+            (
+                latestBlogs?.map((value) => (
+                    <Link key = {value._id} to = {`/blogs/detail/${value._id}`}>
+                    <div className='w-[20rem] flex flex-col gap-2 '>
+                         <img src = {`${BASE_URL}${value.image}`} className='w-[20rem] h-[20rem] rounded-md shadow-lg cursor-pointer object-cover'/>
+                         <h1 className='text-blue font-bold text-xl cursor-pointer duration-500 ease hover:underline'>{value.name}</h1>
+                         <p>{value.text.substring(0, 25)}...</p>
+                        </div>
+                        </Link>
+               ))
+            )}
+           
          </div>
        </div>
     </div>
