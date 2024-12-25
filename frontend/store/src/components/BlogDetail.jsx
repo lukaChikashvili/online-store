c77d039d90452274fb281aa1745e80b229c69b63
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-25 18:17:14 +0400

    add blog detail design change

--- frontend/store/src/components/BlogDetail.jsx
+++ frontend/store/src/components/BlogDetail.jsx
@@ -34,16 +34,17 @@ const BlogDetail = () => {
   return (
     <div className='w-full flex flex-col gap-4 items-center justify-center mt-12'>
         <span onClick = {() => navigate('/blogs')} className='mr-[80rem] underline-offset-4 cursor-pointer text-slate-500 font-bold duration-500 ease-in hover:underline flex gap-4'><ArrowLeft /> უკან დაბრუნება</span>
-       <div className='flex items-center justify-center px-[10rem] gap-[5rem] mt-12'>
-        <div className='flex flex-col gap-12 '>
+       <div className='flex flex-col items-center justify-center px-[10rem] gap-[5rem] mt-12'>
+      
+
+         <img src = {`${BASE_URL}${blogs?.image}`} onError={handleImageError}  className='w-[70rem] h-[20rem] rounded-md shadow-lg object-cover' />
+         
+         <div className='flex flex-col gap-12 '>
         <h1 className='text-center text-3xl text-blue font-bold'>{blogs?.name}</h1>
          <p dangerouslySetInnerHTML={{ __html: blogs?.text }} className='w-full text-xl leading-[2.5rem]'>
      
          </p>
         </div>
-
-         <img src = {`${BASE_URL}${blogs?.image}`} onError={handleImageError}  className='w-[30rem] h-[30rem]  rounded-md shadow-lg' />
-         
        </div>
 
       <Comments />
--- frontend/store/src/components/Trainers.jsx
+++ frontend/store/src/components/Trainers.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { motion } from 'framer-motion'
+import ilia from '../assets/ilia.jpg'
+import leqso from '../assets/leqso.jpg'
 
 const Trainers = () => {
   const trainers = [
     {
       id: 1,
-      img: "https://scontent.fkut1-1.fna.fbcdn.net/v/t39.30808-6/430816657_255384164282674_2009366674719839285_n.jpg?stp=cp6_dst-jpg_p526x296_tt6&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Dxhg3AmuGckQ7kNvgGKnH27&_nc_zt=23&_nc_ht=scontent.fkut1-1.fna&_nc_gid=AZp451lsr6iW6HyGOFri-_x&oh=00_AYCuDOH3Ih0KzXZA7ck--d0K4W7kc6AZC3Z6X6DgmXxQiQ&oe=6768A125",
+      img: leqso,
       name: "ლექსო ტუფურია",
     },
     {
       id: 2,
-      img: "https://scontent.fkut1-1.fna.fbcdn.net/v/t39.30808-6/430040114_256228214198269_7628995155668815644_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=iKOcERKjlsAQ7kNvgE0Z2oD&_nc_zt=23&_nc_ht=scontent.fkut1-1.fna&_nc_gid=AamGozYJodBphIamvlHym3K&oh=00_AYALQZ2Zcq3-RU3a-YkfzoOUL9K8SRllnJbhb1z266wCZg&oe=6768AE93",
+      img: ilia,
       name: "ილია ჩუხუა",
     },
     {
